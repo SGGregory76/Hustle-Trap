50e29736ceccc0c40a26231f4c356599f36d8be2
SGGregory76 <sggregory76@gmail.com>
2025-05-12 04:19:23 -0700

    Create icon-values.js

---
+++ scripts/icon-values.js
@@ -0,0 +1,17 @@
+// icon-values.js
+window.StatIcons = {
+  xp:    { icon:'fa-solid fa-star',        label:'XP'   },
+  rep:   { icon:'fa-solid fa-bullhorn',    label:'Rep'  },
+  rp:    { icon:'fa-solid fa-flask',       label:'RP'   },
+  heat:  { icon:'fa-solid fa-fire',        label:'Heat' },
+  cash:  { icon:'fa-solid fa-dollar-sign', label:'Cash' }
+};
+
+window.IngredientIcons = {
+  'Smoke Bomb':      'fa-solid fa-bomb',
+  'Brass Knuckles':  'fa-solid fa-hand-fist',
+  'Purified Base':   'fa-solid fa-droplet',
+  'Caffeine':        'fa-solid fa-mug-hot',
+  'Lidocaine':       'fa-solid fa-prescription-bottle-medical',
+  // …others…
+};

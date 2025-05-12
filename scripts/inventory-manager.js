f185d44fdc1ff25e10d21796a9ad9693eec466ce
SGGregory76 <sggregory76@gmail.com>
2025-05-11 20:19:00 -0700

    Update inventory-manager.js

--- scripts/inventory-manager.js
+++ scripts/inventory-manager.js
@@ -1,10 +1,26 @@
+// scripts/inventory-manager.js
 window.InventoryManager = {
   _key: i => `ht_inv_${i}`,
-  add: function(item, qty=1) {
+  add(item, qty=1) {
     const curr = JSON.parse(localStorage.getItem(this._key(item))) || 0;
     localStorage.setItem(this._key(item), JSON.stringify(curr + qty));
   },
-  get: function(item) {
+  get(item) {
     return JSON.parse(localStorage.getItem(this._key(item))) || 0;
+  },
+  getAll() {
+    const out = {};
+    Object.keys(localStorage).forEach(k => {
+      if (k.startsWith('ht_inv_')) {
+        const name = k.replace('ht_inv_', '');
+        out[name] = JSON.parse(localStorage.getItem(k)) || 0;
+      }
+    });
+    return out;
+  },
+  reset() {
+    Object.keys(localStorage).forEach(k => {
+      if (k.startsWith('ht_inv_')) localStorage.removeItem(k);
+    });
   }
 };
